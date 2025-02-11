6b4cfcc7e9da24805f53ee0c550650fa01533507
cabagekiller <cabagekiller@gmail.com>
2025-02-11 18:45:56 -0500

    Update stat.c

--- fs/stat.c
+++ fs/stat.c
@@ -18,7 +18,6 @@
 #include <linux/syscalls.h>
 #include <linux/pagemap.h>
 #include <linux/compat.h>
-#include <kernel/ksu.h>
 
 #if defined(CONFIG_KSU_SUSFS_SUS_KSTAT) || defined(CONFIG_KSU_SUSFS_SUS_MOUNT)
 #include <linux/susfs_def.h>
